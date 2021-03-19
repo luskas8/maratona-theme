11e81b56e8c3add619149bd64230c6399febe347
Luskas8 <gnomo1049@gmail.com>
2021-03-19 07:52:04 -0300

    Inicio do theme

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package maratona
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div>
+			<div class="logo">
+				<img src="http://127.0.0.1:8080/wordpress/wp-content/uploads/2021/03/unisantos-horizontal-branco-ai-2.png" alt="Logo unisantos">
+			</div>
+			<ul>
+				<li><a href="#">f</a></li>
+				<li><a href="#">i</a></li>
+				<li><a href="#">t</a></li>
+				<li><a href="#">y</a></li>
+			</ul>
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package maratona
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="container-fluid site">
+	<header id="masthead" class="site-header container-sm">
+		<div class="logo">
+			<img rel="preload" src="http://127.0.0.1:8080/wordpress/wp-content/uploads/2021/03/unisantos-horizontal-azul-ai.png" alt="Logo unisantos">
+		</div>
+		<ul>
+			<li class="active"><a href="#">a maratona</a></li>
+			<li><a href="#">inscrições</a></li>
+			<li><a href="#">programação</a></li>
+			<li><a href="#">ambiente computacional</a></li>
+		</ul>
+	</header><!-- #masthead -->
